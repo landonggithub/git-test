bea1ff2ace2480e64b7ecd1c3a701f677cc8ce57
landonggithub <15036827643@163.com>
2019-01-04 17:00:45 +0800

    创建项目

---
+++ git/src/com/git/GitTest.java
@@ -0,0 +1,30 @@
+/**
+ * Project Name:git
+ * File Name:GitTest.java
+ * Package Name:com.git
+ * Date:2019年1月4日下午4:58:55
+ * Copyright (c) 2019, 1395304172@qq.com All Rights Reserved.
+ *
+*/
+
+package com.git;
+/**
+ * ClassName:GitTest <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2019年1月4日 下午4:58:55 <br/>
+ * @author   Administrator
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class GitTest {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
