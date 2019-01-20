1eea759fd22b9d887c342190bb164b25d9b98f82
landonggithub <15036827643@163.com>
2019-01-20 11:01:35 +0800

    测试分支

--- git/src/com/git/GitTest.java
+++ git/src/com/git/GitTest.java
@@ -23,7 +23,7 @@ public class GitTest {
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
-		System.out.println("<<<<<<<GitTest>>>>>>>>>");
+		System.out.println("<<<<<<<GitTest laf>>>>>>>>>");
 	}
 
 }
